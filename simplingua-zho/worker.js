5ced96f58e585e32f02b95855e6cb65b74e1bb74
lagleki <gleki.is.my.name@gmail.com>
2020-06-06 10:39:23 -0700

    commit

--- simplingua-zho/worker.js
+++ simplingua-zho/worker.js
@@ -5,7 +5,7 @@
     if (bau==='cipra'){bau='en';}
     var cll;
     postMessage({kind: 'loading'});
-    importScripts('bangu.js?sisku=1591460351858','../data/parsed-simplingua-zho.js?sisku=1591460351858', '../sisku.js?sisku=1591460351858');
+    importScripts('bangu.js?sisku=1591465146224','../data/parsed-simplingua-zho.js?sisku=1591465146224', '../sisku.js?sisku=1591465146224');
     postMessage({kind: 'ready'});
     this.onmessage = function(ev) {
       if (ev.data.kind == 'newSearch') {
