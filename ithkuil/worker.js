1cfdf4df248e3b6a2160b390ffa4e4b1e6159830
lagleki <gleki.is.my.name@gmail.com>
2020-06-07 07:28:21 -0700

    commit

--- ithkuil/worker.js
+++ ithkuil/worker.js
@@ -5,7 +5,7 @@
     if (bau==='cipra'){bau='en';}
     var cll;
     postMessage({kind: 'loading'});
-    importScripts('bangu.js?sisku=1591532176371','../data/parsed-ithkuil.js?sisku=1591532176371', '../sisku.js?sisku=1591532176371');
+    importScripts('bangu.js?sisku=1591540085631','../data/parsed-ithkuil.js?sisku=1591540085631', '../sisku.js?sisku=1591540085631');
     postMessage({kind: 'ready'});
     this.onmessage = function(ev) {
       if (ev.data.kind == 'newSearch') {
