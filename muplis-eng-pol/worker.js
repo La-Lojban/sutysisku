e5fbab21c8982cdadc7cabc3806ef8d8108ad994
lagleki <gleki.is.my.name@gmail.com>
2020-06-06 23:46:07 -0700

    commit

--- muplis-eng-pol/worker.js
+++ muplis-eng-pol/worker.js
@@ -5,7 +5,7 @@
     if (bau==='cipra'){bau='en';}
     var cll;
     postMessage({kind: 'loading'});
-    importScripts('bangu.js?sisku=1591512039273','../data/parsed-tatoeba-eng-pol.js?sisku=1591512039273', '../sisku.js?sisku=1591512039273');
+    importScripts('bangu.js?sisku=1591512351862','../data/parsed-tatoeba-eng-pol.js?sisku=1591512351862', '../sisku.js?sisku=1591512351862');
     postMessage({kind: 'ready'});
     this.onmessage = function(ev) {
       if (ev.data.kind == 'newSearch') {
