d9a56754a91436499706403187817cb235d3e521
lagleki <gleki.is.my.name@gmail.com>
2021-11-27 19:11:28 +0000

    fix

---
+++ lojban/769b582553db8b8a123a.js
@@ -0,0 +1,1 @@
+window.nasezvafahi="No definition has been found",window.rimni="la rimni sutysisku",window.bangudecomp="decomposing ...",window.bangubuild="Building index...",window.xuzganalojudri=!0,window.judri="http://jbovlaste.lojban.org/dict/",window.purc="Last searched",window.bangu="english",window.cmene="English";
